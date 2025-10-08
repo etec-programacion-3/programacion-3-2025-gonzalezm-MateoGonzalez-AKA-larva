dd88b8bbd29900d0d277aee260d76a1f8a2ad64c
mateo <ma0.gonzalez@alumno.etec.um.edu.ar>
2025-10-08 12:09:31 -0300

    cambios de issue 3.1, andando

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProductTable from "./components/ProductTable";
+
+function App() {
+  return (
+    <div>
+      <h1>Mi Inventario</h1>
+      <ProductTable />
+    </div>
+  );
+}
+
+export default App;
